cf97b81bf27ba72eefd40e5111776fbc47535338
Willy Koeplin <willykoeplin@gmail.com>
2018-06-08 12:41:46 +0200

    added pictures to cocktail modell and index card

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,8 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
 
+  mount_uploader :photo, PhotoUploader
+
   def all_ingredients
     ingredient_names = []
     self.ingredients.each { |ingredient| ingredient_names << ingredient.name }
